1a4976ead34a9a81fea27664f70f694bd5a91bf2
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-05-14 19:26:45 +0100

    Created Top resturant categories using raw data.js

--- src/Components/HeadlineCards.jsx
+++ src/Components/HeadlineCards.jsx
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import{Resturant} from '../Data/Data'
 
 const HeadlineCards = () => {
+    const [topResturant, setTopResturant] = useState(Resturant)
+
+    // Filter Resturants
+    const filterData = () =>{
+        let data = [...Resturant];
+        data = data.filter((item) => item.tags.toLowerCase() == 'top')
+        setTopResturant(data)
+    }
+
+    // UseEffect
+    useEffect(()=> filterData, [Resturant])
+
   return (
-    <section className='mx-w-5xl mx-auto py-10 px-4'>
+    <section className='py-16 px-4'>
         {/* Container */}
-        <div className=''>
+        <div className='flex items-center justify-between max-w-6xl mx-auto gap-6'>
             {/* Card One */}
-            <div className='relative rounded-xl w-[350px] h-[250px] bg-BreakFast bg-cover bg-no-repeat shadow-xl shadow-slate-950/15'>
-                {/* overlay */}
-                <div className='absolute inset-0 w-full h-full px-4 py-6 text-white bg-black/50 rounded-xl'>
-                    <p className='text-2xl font-semibold uppercase'>OVO resturant</p>
-                    <div className='flex items-center justify-between w-full absolute bottom-4 left-0 px-4'>
-                        <p>15MIN </p>
-                        <button>View Menu</button>
+            { topResturant.map((item)=>( 
+                <div key={item.id} className='relative shadow-xl rounded-xl shadow-slate-950/15'>
+                    {/* overlay */}
+                    <div className='absolute inset-0 w-full h-full px-4 py-6 text-white bg-black/50 rounded-xl'>
+                        <p className='text-2xl font-semibold uppercase'>{item.name}</p>
+                        <div className='flex items-center justify-between w-full absolute bottom-4 left-0 px-4'>
+                            <p>{item.location}</p>
+                            <button>View Menu</button>
+                        </div>                    
                     </div>
-                    
-                </div>
-                
-            </div>
+                    {/* image */}
+                        <img src={item.image} className='w-full max-h-[250px] object-cover rounded-xl' alt="Breakfast"  />
+            </div> 
+            
+            ))};
+            
         </div>
 
     </section>
---
+++ src/Data/Data.js
@@ -0,0 +1,9 @@
+export const Resturant = [
+    {id:1, name:'OVhoe Resturant', category:'Breakfast', location:'Wuse', tags:'Top', image: './src/assets/img/BreakFast.svg' },
+    {id:2, name:'Islamabad Resturant', category:'Lunch', location:'Karu', tags:'Normal', image:'./src/assets/img/Lunch.svg'},
+    {id:3, name:'Hells Kitchen', category:'Dinner', location:'Gwarimpa', tags:'Normal', image:'./src/assets/img/Dinner.svg'},
+    {id:4, name:'Free Chow', category:'Breakfast', location:'Garki', tags:'Normal', image:'./src/assets/img/BreakFast.svg'},
+    {id:5, name:'Zinc House', category:'Lunch', location:'Mpappe', tags:'Top', image:'./src/assets/img/Lunch.svg'},
+    {id:6, name:'Chop Life House', category:'Dinner', location:'Apo', tags:'Top', image:'./src/assets/img/Dinner.svg'},
+];
+
