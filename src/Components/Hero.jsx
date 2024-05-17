470ae12dc1d3db563082f0e40810eda8c25efc71
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-05-17 03:44:15 +0100

    Modified Header styles

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,26 +3,43 @@ import heroImage from '../assets/img/hero-image.svg'
 
 const Hero = () => {
   return (
-    <section className='relative w-full py-24 px-6 mx-auto bg-hero-one bg-no-repeat bg-cover'>
+    <section className='relative w-full px-6 mx-auto bg-hero-one bg-no-repeat bg-cover overflow-hidden'>
         {/* overlay
         <div className='absolute bg-black opacity-40 w-full h-screen'></div> */}
         {/* container */}
-        <div className='flex items-center justify-between max-w-6xl mx-auto px-2 py-4'>
+        <div className='md:grid md:grid-cols-10 flex-col items-center justify-center max-full mx-auto px-2'>
             {/* Column One */}
-            <div className='md:w-1/2'>
-            <h1 className="mb-5 text-6xl font-bold text-slate-800 leading-0">Grow your business
-                <span className='bg-gradient-to-br from-[#9a75dc] via-secColor to-[#9a75dc] 
-                bg-clip-text text-transparent'> we will handle</span> all your logistics
+            <div className='col-span-5 ml-32'>
+            <h1 className="mb-5 text-6xl font-bold text-primaryColor leading-0 ">Hungry!?<br/>
+                <span className='bg-gradient-to-br from-secColor via-bgColor to-secColor 
+                bg-clip-text text-transparent'> Check menu, place order</span> it's that easy
             </h1>
-            <p className="mt-2 mb-5 md:mr-36">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <p className="mt-2 mb-5 md:mr-28">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
             <button className='bg-gradient-to-r from-green-400 to-green-700 px-12 py-4 
                 rounded-md font-medium text-whiteColor text-base' href="#">
-                    Track Package</button>
+                    Order Food </button>
             </div>
 
             {/* Column Two */}
-            <div className='md:w-1/2'>
-                <img src={heroImage} alt="" />
+            <div className='col-span-5 relative md:-ml-36'>
+                {/* container */}
+                <div className='flex items-center translate-x-72'>
+                    {/* circle one */}
+                    <div className='outline-4 outline bg-successColor opacity-85 outline-bgColor 
+                    border-2 border-green-600 outline-offset-8 
+                    rounded-[100%] h-[300px] w-[300px] z-10 -mr-28 '>
+                        <img src="./src/assets/img/banner-image-three.png" alt="" className='' />
+                    </div>
+
+                    {/* Circle two */}
+                    <div className='outline outline-4 outline-successColor 
+                    border-[30px] border-secColor bg-hero-two outline-offset-[20px] rounded-[100%]
+                    h-[700px] w-[700px] -mt-10 -mb-10 bg-no-repeat bg-cover bg-center overflow-hidden'>
+                    <img src=""
+                    className='' alt="" />
+                    </div>
+                </div>
+                
             </div>
 
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,10 @@ export default {
       backgroundImage:{
         "hero-one": "url(./assets/img/hero-one.jpg)",
         "BreakFast": "url(./assets/img/BreakFast.svg)",
+        "hero-two": "url(https://img.freepik.com/free-photo/top-view-table-full-food_23-2149209220.jpg?t=st=1715908891~exp=1715912491~hmac=ff30563622c1778dbf4bcbde71e3269d568c7d195dbdf602459bb8fac7240987&w=900)",
+        "hero-three": "url(https://img.freepik.com/free-psd/urfa-biber-isolated-transparent-background_191095-27948.jpg?t=st=1715905783~exp=1715909383~hmac=413512850de7c0a68a7b144153b0e5cf6eda84ff067ff1a0105a4ff87ef09200&w=740)",
+        "hero-four": "url(https://img.freepik.com/free-photo/close-up-view-delicious-dinner-fried-chicken-dish-with-various-spices-foods-garlics-fallen-oil-bottle-lemon-dried-peppers-mushrooms-kumquats_140725-157903.jpg?t=st=1715906192~exp=1715909792~hmac=e5128c449f3d6d94326d72982fdce0546ce3b99fbb24ecb4bdae9ada316e7360&w=900)"
+
       }
     },
   },
