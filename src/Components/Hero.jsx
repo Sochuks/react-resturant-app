90f09602fe00541b927cce0dd3a2749d83d8ccc1
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-05-18 01:20:12 +0100

    Modify styles for responsive menu and hero image

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,16 +3,16 @@ import heroImage from '../assets/img/hero-image.svg'
 
 const Hero = () => {
   return (
-    <section className='relative w-full px-6 mx-auto bg-hero-one bg-no-repeat bg-cover overflow-hidden'>
+    <section className='relative w-full px-6 pb-14 md:pb-2 -mr-2 md:mr-0 mx-auto bg-hero-one bg-no-repeat bg-cover overflow-hidden'>
         {/* overlay
         <div className='absolute bg-black opacity-40 w-full h-screen'></div> */}
         {/* container */}
-        <div className='md:grid md:grid-cols-10 flex-col items-center justify-center max-full mx-auto px-2'>
+        <div className='md:grid md:grid-cols-10 flex flex-col-reverse items-center justify-center mx-auto px-2'>
             {/* Column One */}
-            <div className='col-span-5 ml-32'>
-            <h1 className="mb-5 text-6xl font-bold text-primaryColor leading-0 ">Hungry!?<br/>
+            <div className='ml-0 text-center md:text-left md:col-span-5 md:ml-32'>
+            <h1 className="mb-5 text-4xl md:text-6xl font-bold text-primaryColor leading-0 ">Hungry!?<br/>
                 <span className='bg-gradient-to-br from-secColor via-bgColor to-secColor 
-                bg-clip-text text-transparent'> Check menu, place order</span> it's that easy
+                bg-clip-text text-transparent'> Check menu, place order</span><br /> it's that easy
             </h1>
             <p className="mt-2 mb-5 md:mr-28">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
             <button className='bg-gradient-to-r from-green-400 to-green-700 px-12 py-4 
@@ -21,20 +21,20 @@ const Hero = () => {
             </div>
 
             {/* Column Two */}
-            <div className='col-span-5 relative md:-ml-36'>
+            <div className='md:col-span-5 relative md:-ml-36'>
                 {/* container */}
-                <div className='flex items-center translate-x-72'>
+                <div className='flex items-center md:translate-x-72'>
                     {/* circle one */}
-                    <div className='outline-4 outline bg-successColor opacity-85 outline-bgColor 
-                    border-2 border-green-600 outline-offset-8 
-                    rounded-[100%] h-[300px] w-[300px] z-10 -mr-28 '>
+                    <div className='hidden outline-2 outline bg-successColor opacity-85 outline-bgColor 
+                    border-2 border-green-600 outline-offset-8 xl:block
+                    rounded-[100%] md:h-[300px] my-10 md:py-2 md:w-[300px] z-10 md:-mr-28 '>
                         <img src="./src/assets/img/banner-image-three.png" alt="" className='' />
                     </div>
 
                     {/* Circle two */}
-                    <div className='outline outline-4 outline-successColor 
-                    border-[30px] border-secColor bg-hero-two outline-offset-[20px] rounded-[100%]
-                    h-[700px] w-[700px] -mt-10 -mb-10 bg-no-repeat bg-cover bg-center overflow-hidden'>
+                    <div className='outline outline-2 outline-successColor shadow-md shadow-darkColor/60 
+                    border-8 md:border-[30px] border-secColor/75 bg-hero-two outline-offset-[20px] rounded-[100%]
+                    md:block h-48 w-48 md:h-[700px] md:w-[700px] mt-10 mb-10 md:-mt-10 md:-mb-10 bg-no-repeat bg-cover bg-center overflow-hidden'>
                     <img src=""
                     className='' alt="" />
                     </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Logo from '../assets/img/logo.png'
 
 const Navbar = () => {
     const [isMenuVisible, toggleMenu] = useState(false)
@@ -10,8 +11,9 @@ const Navbar = () => {
             {/* Column one */}
             <div className='flex items-center justify-between cursor-pointer'>
                 {/* Logo */}
-                <a className='text-2xl py-2 px-2'
-                href="">angryKiitchen</a>
+                <a className='flex items-center text-2xl py-2 px-2' href="">
+                <img src={Logo} alt="" className='h-12 ' />                    
+                angryKiitchen</a>
                 {/* Delivery Filter */}
                 <div className='hidden md:flex items-center bg-gray-200 text-base p-1 rounded-full'>
                     <a className='bg-black text-white rounded-full p-2'>Delivery</a>
@@ -33,7 +35,7 @@ const Navbar = () => {
                     Cart
                 </button>
                 {/* Menu Icon */}
-                <button className="btn btn-square btn-ghost"
+                <button className=""
                 onClick={(e)=>toggleMenu(!isMenuVisible)}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
                 </button>
@@ -45,7 +47,7 @@ const Navbar = () => {
             {isMenuVisible && <div className='bg-black/80 fixed w-full h-screen inset-0 z-10'></div>}
             {/* Side Drawer */}
             <div className={`fixed top-0 -left-[500px] w-[300px] md:w-[400px] h-screen bg-slate-200 
-            px-6 py-6 transition-all ease-in-out duration-500 z-10 ${isMenuVisible ? '-left-[0px]':''}`}>
+            px-6 py-6 transition-all ease-in-out duration-500 z-10 ${isMenuVisible ? 'left-[0px]':''}`}>
                 {/* drawer top */}
                 <div className='flex justify-between mt-2 mb-8'>
                     {/* Logo */}
