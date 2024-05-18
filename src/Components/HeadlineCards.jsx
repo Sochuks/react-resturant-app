5a3c27431756cb35a4071fb99e89608f23a18cef
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-05-18 07:11:54 +0100

    created rating component - Modify HeadlineCards styles

--- src/Components/HeadlineCards.jsx
+++ src/Components/HeadlineCards.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import{Resturant} from '../Data/Data'
+import Rating from './Rating'
 
 const HeadlineCards = () => {
     const [topResturant, setTopResturant] = useState(Resturant)
@@ -15,23 +16,61 @@ const HeadlineCards = () => {
     useEffect(()=> filterData, [Resturant])
 
   return (
-    <section className='py-16 px-4'>
+    <section className='py-16 px-2'>
         {/* Container */}
-        <div className='flex items-center justify-between max-w-6xl mx-auto gap-6'>
+        <div className='flex flex-col md:flex-row max-w-6xl mx-auto gap-10 overflow-hidden'>
             {/* Card One */}
             { topResturant.map((item)=>( 
-                <div key={item.id} className='relative shadow-xl rounded-xl shadow-slate-950/15'>
-                    {/* overlay */}
-                    <div className='absolute inset-0 w-full h-full px-4 py-6 text-white bg-black/50 rounded-xl'>
-                        <p className='text-2xl font-semibold uppercase'>{item.name}</p>
-                        <div className='flex items-center justify-between w-full absolute bottom-4 left-0 px-4'>
-                            <p>{item.location}</p>
-                            <button>View Menu</button>
-                        </div>                    
+                
+                <article key={item.id} className='flex flex-wrap md:flex-nowrap cursor-pointer transform duration-500 hover:-translate-y-1'>
+                    <img className="w-full max-h-[400px] object-cover md:w-52" src="https://i.ibb.co/Kr4b0zJ/152013403-10158311889099633-8423107287930246533-o.jpg" alt="" />
+                    
+                    <div>
+                        {/* top half */}
+                        <div className="p-5 pb-10">
+                            <h1 className="text-2xl font-semibold text-gray-800 mt-4">
+                                {item.name}
+                            </h1>
+                            <p className="text-xl text-gray-400 mt-2 leading-relaxed">
+                                Located in {item.location} Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            </p>
+                        </div>
+
+                        {/* Bottom half */}
+                        <div className='p-5 bg-secColor'>
+                            {/* container */}
+                            <div className='sm:flex sm:justify-between'>
+                                {/* Location & rating */}
+                                <div>
+                                    <div className="text-lg text-gray-700">
+                                        <span className="text-gray-900 font-bold">196 km</span> from Dhaka
+                                    </div>
+                                    {/* Ratings */}
+                                    <div className='flex items-center justify-between'>
+                                        {/* stars */}
+                                        <div className='flex items-center'>
+                                            <Rating rating={item.rating} maxRating={5}/>
+                                        </div>
+                                        {/* total votes */}
+                                        <div className="text-gray-600 ml-2 text-sm md:text-base mt-1">
+                                            16 reviews
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* Button */}
+                                <button className="mt-3 sm:mt-0 py-2 px-5 md:py-3 md:px-6 bg-purple-700 hover:bg-purple-600 font-bold text-white md:text-lg rounded-lg shadow-md">
+                                    Menu
+                                </button>
+                            </div>
+                                {/* BOTTOM TEXT */}
+                                <div className="mt-3 text-gray-600 text-sm md:text-sm">
+                                    *Places to visit: Mahasthangarh, Vasu Bihar &amp; Momo Inn
+                                </div>
+                        </div>
+                        
                     </div>
-                    {/* image */}
-                        <img src={item.image} className='w-full max-h-[250px] object-cover rounded-xl' alt="Breakfast"  />
-            </div> 
+                    
+                </article>
             
             ))};
             
--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,10 +1,10 @@
 export const Resturant = [
-    {id:1, name:'OVhoe Resturant', category:'Breakfast', location:'Wuse', tags:'Top', image: './src/assets/img/BreakFast.svg' },
-    {id:2, name:'Islamabad Resturant', category:'Lunch', location:'Karu', tags:'Normal', image:'./src/assets/img/Lunch.svg'},
-    {id:3, name:'Hells Kitchen', category:'Dinner', location:'Gwarimpa', tags:'Normal', image:'./src/assets/img/Dinner.svg'},
-    {id:4, name:'Free Chow', category:'Breakfast', location:'Garki', tags:'Normal', image:'./src/assets/img/BreakFast.svg'},
-    {id:5, name:'Zinc House', category:'Lunch', location:'Mpappe', tags:'Top', image:'./src/assets/img/Lunch.svg'},
-    {id:6, name:'Chop Life House', category:'Dinner', location:'Apo', tags:'Top', image:'./src/assets/img/Dinner.svg'},
+    {id:1, name:'OVhoe Resturant', category:'Breakfast', rating: 5, location:'Wuse', tags:'Normal', image: './src/assets/img/BreakFast.svg' },
+    {id:2, name:'Islamabad Resturant', category:'Lunch', rating: 3, location:'Karu', tags:'Normal', image:'./src/assets/img/Lunch.svg'},
+    {id:3, name:'Hells Kitchen', category:'Dinner', rating: 4, location:'Gwarimpa', tags:'Normal', image:'./src/assets/img/Dinner.svg'},
+    {id:4, name:'Free Chow', category:'Breakfast',  rating: 3, location:'Garki', tags:'Normal', image:'./src/assets/img/BreakFast.svg'},
+    {id:5, name:'Zinc House', category:'Lunch', rating: 5, location:'Mpappe', tags:'Top', image:'./src/assets/img/Lunch.svg'},
+    {id:6, name:'Chop Life House', category:'Dinner', rating: 4, location:'Apo', tags:'Top', image:'./src/assets/img/Dinner.svg'},
 ];
 
 export const Menu = [
